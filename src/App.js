c3f0e118fb7cb5e55211c61de9ba570affdc25b1
Odette Gleen <nkargleeny@gmail.com>
2022-11-04 09:15:16 +0100

    set up the bookstore and its components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Navbar';
+import Book from './components/Book';
+import Categories from './components/Categories';
 
-function App() {
+const App = () => {  //eslint-disable-line
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Routes>
+        <Route path="/Book" element={<Book />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => { //eslint-disable-line
+  return (
+    <div className="navBar">
+      <h1 className="logo">Bookstore CMS</h1>
+      <ul className="navItems">
+        <li className="navItem"><Link to="/Book">Book</Link></li>
+        <li className="navItem"><Link to="/calculate">Categories</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
