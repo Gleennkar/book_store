00ec7d620a32a7919c2f70ed04f03cba658859e7
Odette Gleen <nkargleeny@gmail.com>
2022-11-12 20:13:54 +0100

    connecting the api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,54 +1,117 @@
 // import { isHtmlElement } from "react-router-dom/dist/dom";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 import types from '../types/types';
 
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/AeyELqRTznXa1ZPd7jWT/books';
+
 const initialState = {
-  Booklist: [
-    {
-      id: 1,
-      title: 'My Tech Journey',
-      author: 'Gleeny',
-    },
-    {
-      id: 2,
-      title: 'What You Say',
-      author: 'Oprah',
-    },
-    {
-      id: 3,
-      title: 'Unlock It',
-      author: 'Dan Lok',
-    },
-  ],
+  isLoading: true,
+  Booklist: {},
+  //   Booklist: [
+  //     {
+  //       id: 1,
+  //       title: 'My Tech Journey',
+  //       author: 'Gleeny',
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'What You Say',
+  //       author: 'Oprah',
+  //     },
+  //     {
+  //       id: 3,
+  //       title: 'Unlock It',
+  //       author: 'Dan Lok',
+  //     },
+  //   ],
 
 };
-export const addBooks = (book) => ({
-  type: types.ADD_BOOK,
-  payload: book,
+
+export const getBooks = createAsyncThunk(
+  types.BOOKS_FETCHED,
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+
+);
+export const addBooks = createAsyncThunk(
+  types.ADD_BOOK,
+  async (payload, thunkAPI) => {
+    await axios.post(url, payload);
+    return thunkAPI.dispatch(getBooks());
+  },
+
+);
+
+export const removeBook = createAsyncThunk(
+  types.REMOVE_BOOK,
+  async (payload, thunkAPI) => {
+    await axios.delete(`${url}/${payload}`);
+    return thunkAPI.dispatch(getBooks());
+  },
+);
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => ({ ...state, bookList: [...state.Booklist, action.bookDetail] }),
+    removeBook: (state, action) => ({
+      ...state,
+      Booklist: [
+        ...state.Booklist.filter((book) => book.id !== action.index.id),
+      ],
+    }),
+  },
+
+  extraReducers: {
+    [getBooks.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getBooks.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.bookList = action.payload;
+    },
+    [getBooks.rejected]: (state) => {
+      state.isLoading = true;
+    },
+  },
 });
 
-export const removeBook = (payload) => ({
-  type: types.REMOVE_BOOK,
-  payload,
-});
+// export const addBooks = (book) => ({
+//   type: types.ADD_BOOK,
+//   payload: book,
+// });
 
-// reducer
+// export const removeBook = (payload) => ({
+//   type: types.REMOVE_BOOK,
+//   payload,
+// });
 
-const booksReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case types.ADD_BOOK:
-      return { ...state, Booklist: [...state.Booklist, action.payload] };
+// // reducer
 
-    case types.REMOVE_BOOK:
-      return {
-        ...state,
-        Booklist: [
-          ...state.Booklist.filter((book) => book.id !== action.payload),
-        ],
-      };
+// const booksReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case types.ADD_BOOK:
+//       return { ...state, Booklist: [...state.Booklist, action.payload] };
 
-    default:
-      return state;
-  }
-};
+//     case types.REMOVE_BOOK:
+//       return {
+//         ...state,
+//         Booklist: [
+//           ...state.Booklist.filter((book) => book.id !== action.payload),
+//         ],
+//       };
 
-export default booksReducer;
+//     default:
+//       return state;
+//   }
+// };
+
+export default bookSlice.reducer;
