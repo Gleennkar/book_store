61af6a136ae17b455224443f9ed2dac471a09c4c
Odette Gleen <nkargleeny@gmail.com>
2022-11-13 01:21:08 +0100

    fix linter errors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -16,7 +16,6 @@ export const getBooks = createAsyncThunk(
   async (thunkAPI) => {
     try {
       const response = await axios.get(url);
-      console.log(response.data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data);
