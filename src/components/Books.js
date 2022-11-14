6c41d2b6181e1639fdcbcc05e396f7b085888310
Odette Gleen <nkargleeny@gmail.com>
2022-11-14 17:37:59 +0100

    styling

--- src/components/Books.js
+++ src/components/Books.js
@@ -23,27 +23,27 @@ function Books() {
           <div className="flex-col mt-3 book" key={uuidv4()}>
             <h2 className="book-title">{library.Booklist[bookItem][0].title}</h2>
             <p className="book-author">{library.Booklist[bookItem][0].author}</p>
-            <ul className="book-buttons flex-row">
+            <ul className="book-buttons d-flex">
               <li className="book-button d-flex mt-2 w-90"><button className=" d-flex btn-book" type="button">Comments</button></li>
               <li className="book-button-fluid d-flex mt-2 w-90">
                 <button type="button" id={bookItem} className=" d-flex remove-btn" onClick={(e) => { handleRemove(e.target.id); }}>Remove</button>
               </li>
-              <li className="book-button-fluid d-flex mt-2 w-90"><button className="d-flex btn-book" type="button">Edit</button></li>
+              <li className="book-button-fluid d-flex mt-2 w-90"><button className="d-flex btn-book1" type="button">Edit</button></li>
             </ul>
           </div>
 
         ))}
 
-        <div className="cirPro flex-row">
+        <div className="cirPro d-flex">
           <div className="oval">X</div>
           <div className="progress-container">
             <p className="perc">64%</p>
             <p className="completed">Completed</p>
           </div>
         </div>
-        <div className="progress">
+        <div className="progress flex-column">
           <p className="chap-1">CURRENT CHAPTER</p>
-          <p className="chap-2">Chaper 17</p>
+          <p className="chap-2">Chapter 17</p>
           <button className="btn-chapter" type="button">UPDATE PROGRESS</button>
         </div>
 
